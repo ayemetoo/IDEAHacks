847bd4a3c04f72b429cc8d88f2849c38ee5da722
Ben Yang <thisisbenbtw@gmail.com>
2018-01-13 14:27:56 -0800

    added teensy2

--- IDEAHacksTeensy2/IDEAHacksTeensy2.ino
+++ IDEAHacksTeensy2/IDEAHacksTeensy2.ino
@@ -9,7 +9,7 @@
 #define CE 9
 
 Adafruit_HTU21DF htu = Adafruit_HTU21DF(); //temp sensor
-RF24 rf(CE, CSN); //radio module
+//RF24 rf(CE, CSN); //radio module
 
 struct tempHumd {
   float temp;
@@ -19,13 +19,14 @@ struct tempHumd {
 int target = 70;
 
 void setup() {
-  rf.setChannel(5);
-  rf.setPALevel(RF24_PA_MIN);
-  rf.openWritingPipe(0xc2c2c2c2c2); //what do i put here
-  rf.openReadingPipe(1, 0xe7e7e7e7e7);
-  rf.setCRCLength(RF24_CRC_16);
+//  rf.setChannel(5);
+//  rf.setPALevel(RF24_PA_MIN);
+//  rf.openWritingPipe(0xc2c2c2c2c2); //what do i put here
+//  rf.openReadingPipe(1, 0xe7e7e7e7e7);
+//  rf.setCRCLength(RF24_CRC_16);
   
   Serial.begin(9600);
+  Serial.println("ss");
   
   if (!htu.begin()) {
     Serial.println("Couldn't find sensor!");
@@ -35,15 +36,16 @@ void setup() {
 
 
 void loop() {
+  Serial.println("aa");
   //set struct
   inside.temp = CtoF(htu.readTemperature());
   inside.humd = htu.readHumidity();
   
   //recieve from arduino
-  rf.startListening();
-  if (rf.available()) {
-    rf.read(&outside, sizeof(float)*2);  
-  }
+//  rf.startListening();
+//  if (rf.available()) {
+//    rf.read(&outside, sizeof(float)*2);  
+//  }
   
   Serial.println("Inside--------");
   Serial.print("Temp: "); Serial.print(inside.temp);
